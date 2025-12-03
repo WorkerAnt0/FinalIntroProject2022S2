3b1d06e0c8add0b39cae72e499556cd665f42dd6
Lazarus <104911585+WorkerAnt0@users.noreply.github.com>
2025-12-03 18:00:32 -0500

    fixing stuff

--- sketch.js
+++ sketch.js
@@ -20,6 +20,17 @@ loadSprite("Space ship", "ship5-Sheet.png", {
     },
   },
 });
+loadSprite("Communication", "AlienCom1.png", {
+  sliceX: 3,
+  sliceY: 0,
+  anims: {
+    run: {
+      from: 0,
+      to: 100,
+      speed: 10,
+    },
+  },
+});
 loadSprite("Fireball", "Fireball-Sheet.png", {
   sliceX: 27,
   sliceY: 0,
@@ -234,6 +245,12 @@ onLoad(() => {
       pos(width() / 100, height() / 1.7),
       z(2),
     ]);
+	  add([
+      sprite("Communication", { anim: "run" }),
+      scale(3.5),
+      opacity(0.98),
+      pos(width() / 100, height() / 1.7),
+    ]);
     add([
       text("Space Invaders", {
         font: "KH",
@@ -327,6 +344,12 @@ onLoad(() => {
         });
       }
     });
+	add([
+      sprite("textbox", { anim: "run" }),
+      scale(1.5),
+      pos(width() / 2.4, height() / 1500),
+      z(2),
+    ]);
     /*
     //Dialogue
     add([
@@ -334,12 +357,6 @@ onLoad(() => {
       scale(15),
       pos(width() / 2.2, height() / 20),
       z(3),
-    ]);
-    add([
-      sprite("textbox", { anim: "run" }),
-      scale(1.5),
-      pos(width() / 2.4, height() / 1500),
-      z(2),
     ]);
     onDraw(() => {
       add([
